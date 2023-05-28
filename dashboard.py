3d58c80b2aff6220ee2c924e41e1599ecab9da1f
Mario Avolio <64833154+MarioAvolio@users.noreply.github.com>
2023-05-28 23:28:25 +0200

    upgrade

--- dashboard.py
+++ dashboard.py
@@ -5,17 +5,65 @@ import dash_bootstrap_components as dbc
 from dash.dependencies import Output, Input
 import plotly.graph_objects as go
 import pandas as pd
+import dash_core_components as dcc
+from jupyter_dash import JupyterDash
+from dash_bootstrap_templates import load_figure_template
 
-MODELS="Models"
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
+load_figure_template("LUX")
+
+
+MODELS = "Models"
+MODELS = "Models"
+
+
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "24rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+
+sidebar = html.Div(
+    [
+        html.H2("Filters"),
+        html.Hr(),
+        html.P("Choose a filter", className="lead"),
+        dbc.Nav(
+            [
+                dcc.Dropdown(
+                    id="Type_of_research",
+                    options=[{"label": MODELS, "value": MODELS}],
+                    placeholder="Select a type of research",
+                    # style={"width": "50%"}
+                ),
+                html.Br(),
+                dcc.Dropdown(id="two"),
+                html.Br(),
+                dcc.Dropdown(id="three"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+
+app = dash.Dash(
+    __name__, external_stylesheets=[dbc.themes.LUX], suppress_callback_exceptions=True
+)
+server = app.server
 
 
 # Import pandas, use it to open the poverty dataset, and assign it to the variable
-f_1_data = pd.read_csv('f1_data.csv')
+f_1_data = pd.read_csv("f1_data.csv")
 
-dict_of_data = {
-    MODELS: f_1_data
-}
+dict_of_data = {MODELS: f_1_data}
+
 
 # If we run the app now, we will get some empty figures with a white background for the
 # ones where we did not set default values. For those, we also need to create empty figures,
@@ -27,119 +75,257 @@ dict_of_data = {
 # such figures whenever we want them. The following code achieves that:
 def make_empty_fig():
     fig = go.Figure()
-    fig.layout.paper_bgcolor = '#E5ECF6'
-    fig.layout.plot_bgcolor = '#E5ECF6'
+    fig.layout.paper_bgcolor = "#E5ECF6"
+    fig.layout.plot_bgcolor = "#E5ECF6"
     return fig
 
 
+app.layout = html.Div(
+    children=[
+        dbc.Row(
+            [
+                dbc.Col(),
+                dbc.Col(
+                    [
+                        html.H1(
+                            "Amazon Fine Food Reviews", style={"textAlign": "center"}
+                        ),
+                        html.H2(
+                            "Sentiment Analysis from BoW to DistilBERT Transformers",
+                            style={"textAlign": "center"},
+                        ),
+                        
+                        html.Div(
+                            [
+                                dbc.Tabs(
+                                    [
+                                        dbc.Tab(
+                                            [
+                                                html.Ul(
+                                                    [
+                                                        html.Br(),
+                                                        html.Li(
+                                                            "Number of Economies: 170"
+                                                        ),
+                                                        html.Li(
+                                                            "Temporal Coverage: 1974 - 2019"
+                                                        ),
+                                                        html.Li(
+                                                            "Update Frequency: Quarterly"
+                                                        ),
+                                                        html.Li(
+                                                            "Last Updated: March 18, 2020"
+                                                        ),
+                                                        html.Li(
+                                                            [
+                                                                "Source: ",
+                                                                html.A(
+                                                                    "https://datacatalog.worldbank.org/dataset/poverty-and-equity-database",
+                                                                    href="https://datacatalog.worldbank.org/dataset/poverty-and-equity-database",
+                                                                ),
+                                                            ]
+                                                        ),
+                                                    ]
+                                                )
+                                            ],
+                                            label="Key Facts",
+                                        ),
+                                        dbc.Tab(
+                                            [
+                                                html.Ul(
+                                                    [
+                                                        html.Br(),
+                                                        html.Li(
+                                                            "Project title: Amazon Fine Food Reviews - Sentiment Analysis from BoW to DistilBERT Transformers"
+                                                        ),
+                                                        html.Li(
+                                                            [
+                                                                "GitHub repo: ",
+                                                                html.A(
+                                                                    "https://github.com/MarioAvolio/Amazon-Fine-Foods-reviews-Transformers-Text-Classification",
+                                                                    href="https://github.com/MarioAvolio/Amazon-Fine-Foods-reviews-Transformers-Text-Classification",
+                                                                ),
+                                                            ]
+                                                        ),
+                                                    ]
+                                                )
+                                            ],
+                                            label="Project Info",
+                                        ),
+                                    ],
+                                ),
+                            ],
+                            # style={"padding-right": "211px", "margin-top": "7px"},
+                        ),
+                    ],
+                    width=9,
+                    style={"margin-left": "7px", "margin-top": "7px"},
+                ),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(sidebar),
+                dbc.Col(
+                    width=9,
+                    style={
+                        "margin-left": "15px",
+                        "margin-top": "7px",
+                        "margin-right": "15px",
+                    },
+                ),
+            ]
+        ),
+        # html.Div(
+        #     [
+        #         dbc.Label("Type of research:"),
+        #         dbc.Col(
+        #             [
+        #                 dcc.Dropdown(
+        #                     id="Type_of_research",
+        #                     options=[{"label": MODELS, "value": MODELS}],
+        #                     placeholder="Select a type of research",
+        #                     style={"width": "50%"},
+        #                 ),
+        #                 html.Br(),
+        #                 html.Div(id="report"),
+        #                 html.Br(),
+        #                 # dcc.Graph(id='gini_year_barchart')
+        #             ],
+        #             # md=12,
+        #             # lg=4,
+        #         ),
+        #         dcc.Loading(
+        #             [
+        #                 html.Div(
+        #                     id="output",
+        #                     children=[],
+        #                     # style={"width": "50%"},
+        #                 )
+        #             ]
+        #         ),
+        #         dbc.Tabs(
+        #             [
+        #                 dbc.Tab(
+        #                     [
+        #                         html.Ul(
+        #                             [
+        #                                 html.Br(),
+        #                                 html.Li("Number of Economies: 170"),
+        #                                 html.Li("Temporal Coverage: 1974 - 2019"),
+        #                                 html.Li("Update Frequency: Quarterly"),
+        #                                 html.Li("Last Updated: March 18, 2020"),
+        #                                 html.Li(
+        #                                     [
+        #                                         "Source: ",
+        #                                         html.A(
+        #                                             "https://datacatalog.worldbank.org/dataset/poverty-and-equity-database",
+        #                                             href="https://datacatalog.worldbank.org/dataset/poverty-and-equity-database",
+        #                                         ),
+        #                                     ]
+        #                                 ),
+        #                             ]
+        #                         )
+        #                     ],
+        #                     label="Key Facts",
+        #                 ),
+        #                 dbc.Tab(
+        #                     [
+        #                         html.Ul(
+        #                             [
+        #                                 html.Br(),
+        #                                 html.Li(
+        #                                     "Project title: Amazon Fine Food Reviews - Sentiment Analysis from BoW to DistilBERT Transformers"
+        #                                 ),
+        #                                 html.Li(
+        #                                     [
+        #                                         "GitHub repo: ",
+        #                                         html.A(
+        #                                             "https://github.com/MarioAvolio/Amazon-Fine-Foods-reviews-Transformers-Text-Classification",
+        #                                             href="https://github.com/MarioAvolio/Amazon-Fine-Foods-reviews-Transformers-Text-Classification",
+        #                                         ),
+        #                                     ]
+        #                                 ),
+        #                             ]
+        #                         )
+        #                     ],
+        #                     label="Project Info",
+        #                 ),
+        #             ]
+        #         ),
+        #     ],
+        #     style={"marginLeft": 20, "marginRight": 20},
+        # ),
+    ],
+)
 
-app.layout = html.Div([
-    html.H1('Amazon Fine Food Reviews', style={'textAlign': 'center'}),
-    html.H2('Sentiment Analysis from BoW to DistilBERT Transformers', style={'textAlign': 'center'}),
-    html.H3('Choose the type of research:', style={'textAlign': 'center'}),
-    dbc.Row([
-        dbc.Label("Type of research:"),
-        dbc.Col([
-            dcc.Dropdown(id='Type_of_research',
-                options=[{'label': MODELS , 'value': MODELS}], 
-                placeholder="Select a type of research",),
-            html.Br(),
-            html.Div(id='report'),
-            html.Br(), 
-            
-            # dcc.Graph(id='gini_year_barchart')
-        ]),
-        
-        dbc.Col([
-            # dbc.Label("Models:"),
-            dcc.Dropdown(id='Models',
-                         placeholder="Select a model",
-                        options=[{'label': "Bag-of-words", 'value': "BoW"},
-                                {'label': "Word2vec", 'value': "W2V"}, 
-                                {'label': "DistilBERT", 'value': "DB"}, {'label': "All", 'value': ""}]),
-            html.Br(),
-            dcc.Graph(id='f1_chart', figure=make_empty_fig()),
-    
-        ], md=12, lg=8), # Another thing we need to handle is how the resizing of the browser window affects the
-# size and placement of our different components. The figures are responsive by default, but
-# we need to make some decisions for the figures that are placed side by side
-        
-    ]),
-   
-    
-    dbc.Tabs([
-       dbc.Tab([
-           html.Ul([
-               html.Br(),
-               html.Li('Number of Economies: 170'),
-               html.Li('Temporal Coverage: 1974 - 2019'),
-               html.Li('Update Frequency: Quarterly'),
-               html.Li('Last Updated: March 18, 2020'),
-               html.Li([
-                   'Source: ',
-                   html.A('https://datacatalog.worldbank.org/dataset/poverty-and-equity-database',
-                          href='https://datacatalog.worldbank.org/dataset/poverty-and-equity-database')
-               ])
-           ])
 
-       ], label='Key Facts'),
-        dbc.Tab([
-            html.Ul([
+@app.callback(Output("output", "children"), Input("Type_of_research", "value"))
+def display_col(type_of_research):
+    if type_of_research is None:
+        return html.H3("")
+    if type_of_research == MODELS:
+        return dbc.Col(
+            [
+                # dbc.Label("Models:"),
+                dcc.Dropdown(
+                    id="Models",
+                    placeholder="Select a model",
+                    options=[
+                        {"label": "Bag-of-words", "value": "BoW"},
+                        {"label": "Word2vec", "value": "W2V"},
+                        {"label": "DistilBERT", "value": "DB"},
+                        {"label": "All", "value": ""},
+                    ],
+                    style={"width": "50%"},
+                ),
                 html.Br(),
-                html.Li('Project title: Amazon Fine Food Reviews - Sentiment Analysis from BoW to DistilBERT Transformers'),
-                html.Li(['GitHub repo: ',
-                         html.A('https://github.com/MarioAvolio/Amazon-Fine-Foods-reviews-Transformers-Text-Classification',
-                                href='https://github.com/MarioAvolio/Amazon-Fine-Foods-reviews-Transformers-Text-Classification')
-                         ])
-            ])
-        ], label='Project Info')
-    ]),
-
-    
-], style={'backgroundColor': '#E5ECF6'}) # Another thing we can consider doing is making our theme consistent with the theme
-# of the charts that we are using. We can set the background color of the app to the same
-# default color used in Plotly's figures. 
+                dcc.Loading([dcc.Graph(id="f1_chart", figure=make_empty_fig())]),
+            ],
+            # md=3,
+            # lg=3,
+            id="col2",
+        )
 
 
-@app.callback(Output('report', 'children'),
-              Input('Type_of_research', 'value'))
+@app.callback(Output("report", "children"), Input("Type_of_research", "value"))
 def display_report(type_of_research):
     if type_of_research is None:
-        return ''
+        return ""
 
-    return [html.H3(type_of_research),
-            f'This analysis is based on the value of f1-score-weighted for each model. The best model is DistilBERT Fine-Tuned with value of {f_1_data["f1-score-weighted"].max()}. Choose the method for text representation to see the models results.']
+    return [
+        html.H3(type_of_research),
+        f'This analysis is based on the value of f1-score-weighted for each model. The best model is DistilBERT Fine-Tuned with value of {f_1_data["f1-score-weighted"].max()}. Choose the method for text representation to see the models results.',
+    ]
 
 
-
-@app.callback(Output('f1_chart', 'figure'), Input('Models', 'value'))
+@app.callback(
+    Output("f1_chart", "figure"),
+    Input("Models", "value"),
+    suppress_callback_exceptions=True,
+)
 def plot_f_1_data(model_type):
     fig = go.Figure()
-    data_to_plot = f_1_data[f_1_data['name-method'].str.contains(str(model_type))]
-    fig.add_bar(x=data_to_plot['name-method'],
-                y=data_to_plot['f1-score-weighted'])
-    
-    if model_type == '':
-        title = 'F1-score-weighted for all models'
+    data_to_plot = f_1_data[f_1_data["name-method"].str.contains(str(model_type))]
+    fig.add_bar(x=data_to_plot["name-method"], y=data_to_plot["f1-score-weighted"])
+
+    if model_type == "":
+        title = "F1-score-weighted for all models"
     elif model_type is not None:
-        title = f'F1-score-weighted for models based on {model_type}'
+        title = f"F1-score-weighted for models based on {model_type}"
     else:
-        title=''
-    
-#     The bigger rectangle enclosing the smaller one is the "paper" area. In the charts we have
-# produced so far, it has been colored white. We can also set its color to the same color,
-# making all background colors the same for our app. We simply have to add the following
-# line to the callback functions that generate charts
-    fig.layout.paper_bgcolor = '#E5ECF6' 
+        title = ""
+
+    #     The bigger rectangle enclosing the smaller one is the "paper" area. In the charts we have
+    # produced so far, it has been colored white. We can also set its color to the same color,
+    # making all background colors the same for our app. We simply have to add the following
+    # line to the callback functions that generate charts
+    fig.layout.paper_bgcolor = "#E5ECF6"
     fig.layout.title = title
     # fig.layout.template = 'simple_white'
 
     return fig
 
 
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
